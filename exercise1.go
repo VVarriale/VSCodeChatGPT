df707d05cb22a206d8ccdbdab504954c080a56c8
VVarriale <33053088+VVarriale@users.noreply.github.com>
2025-11-25 14:48:07 +0100

    Add files via upload

---
+++ exercise1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func (v *Vertex) String() string {
+	return fmt.Sprintf("(%d,%d)", v.X, v.Y)
+}
+
+func printSomething(s fmt.Stringer) {
+	fmt.Println(s.String())
+}
+
+func main() {
+	v := &Vertex{X: 10, Y: 20}
+	printSomething(v)
+}
